c23ef1b46e96f1aaa71ba8a8c3b9ad2248573032
Petko Petrov <petko.petrov89@gmail.com>
2017-12-01 18:07:15 +0200

    simple multiply

---
+++ First_steps_with_JS/multiply.js
@@ -0,0 +1,13 @@
+function solved(x, y) {
+    let result = multiply(x, y);
+
+    console.log(result);
+}
+function multiply(a, b) {
+    a = a || 1;
+    b = b || 1;    // to prevent case 0 and NaN result
+
+    return a * b;
+}
+
+solved(6, 10);
