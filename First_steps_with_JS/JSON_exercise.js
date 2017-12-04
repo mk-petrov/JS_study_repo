5fafac43aa6c6725a2594be380de45f1f83a0891
Petko Petrov <petko.petrov89@gmail.com>
2017-12-04 18:30:15 +0200

    js simple exercise

---
+++ First_steps_with_JS/JSON_exercise.js
@@ -0,0 +1,17 @@
+let obj = {};
+//add properties to an object
+obj['name'] = 'Ivan';
+obj.salary = 10000;
+
+let obj2 = {};
+obj2.lastName = 'Ivanov';
+
+//create an object
+let obj3 = {
+    name: 'Saimon'
+};
+
+//create an array
+let arr = [1, 2, 3];
+
+console.log(obj.name + ' ' + obj2.lastName);
