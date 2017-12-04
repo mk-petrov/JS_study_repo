be8cf39314fa33aca2bf8a4ff1d7f57eda2c9a55
Petko Petrov <petko.petrov89@gmail.com>
2017-12-04 23:22:53 +0200

    added new js file

--- First_steps_with_JS/JSON_exercise.js
+++ First_steps_with_JS/JSON_exercise.js
@@ -3,15 +3,43 @@ let obj = {};
 obj['name'] = 'Ivan';
 obj.salary = 10000;
 
+// delete the property
+delete obj.salary;
+
 let obj2 = {};
 obj2.lastName = 'Ivanov';
 
 //create an object
 let obj3 = {
-    name: 'Saimon'
+    name: 'Saimon',
+    sayHello: function(){
+        return 'Hello my name is ' + this.name;
+    }
 };
 
 //create an array
 let arr = [1, 2, 3];
 
 console.log(obj.name + ' ' + obj2.lastName);
+console.log(obj3.sayHello());
+
+let object = {
+    name: 'Stefan',
+    age: 19,
+    favColors: [
+        'blue',
+        'green',
+        'yellow'
+    ],
+    favDrink: 'bier'
+};
+
+// coverts the obj as string
+let objectAsString = JSON.stringify(object);
+
+console.log(objectAsString);
+
+// from string to object again
+let parsedObject = JSON.parse(objectAsString);
+
+console.log(parsedObject.name);
