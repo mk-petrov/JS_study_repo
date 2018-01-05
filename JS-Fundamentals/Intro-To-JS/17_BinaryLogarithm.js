de23d28af0d503ad471a739ab8ce905bf7a1f367
Petko Petrov <petko.petrov89@gmail.com>
2018-01-05 11:52:53 +0200

    added files

---
+++ JS-Fundamentals/Intro-To-JS/17_BinaryLogarithm.js
@@ -0,0 +1,8 @@
+function solve(args) {
+    for (let num of args) {
+        console.log(Math.log2(num));
+    }
+}
+
+solve([1024, 1048576, 256, 1, 2]);
+
---
+++ JS-Fundamentals/Intro-To-JS/18_PrintTriangle.js
@@ -0,0 +1,10 @@
+function solve(n) {
+    for (let i = 1; i <= n; i++) {
+        console.log('*'.repeat(i));
+    }
+    for (let i = n - 1; i > 0; i--) {
+        console.log('*'.repeat(i));
+    }
+}
+
+solve(3);
