f3f4f01db67920011ddb12277c29a3a33aece905
mk-petrov <petko.petrov89@gmail.com>
2018-05-15 12:50:38 +0200

    new js file

---
+++ JS-Advanced/Error_Handling/01_subsum.js
@@ -0,0 +1,19 @@
+function subsum(arr, startIndex, endIndex) {
+    if(!Array.isArray(arr)){
+        return NaN;
+    }
+    if(startIndex < 0) startIndex = 0;
+    if(endIndex >= arr.length) endIndex = arr.length - 1;
+    let sum = 0;
+    for (let i = startIndex; i <= endIndex; i++) {
+        sum += Number(arr[i]);
+    }
+    return sum;
+}
+
+console.log(subsum([10, 20, 30, 40], -5, 2));  //60
+console.log(subsum([10, 20, 30, 40], 1, 20));  //90
+console.log(subsum([10, 20, 30, 40], 1, 2));   //50
+console.log(subsum('pesho', 1, 2));            //NaN
+console.log(subsum({}, 1, 2));                 //NaN
+console.log(subsum(['10', '20', '30'], 1, 2));  //50
