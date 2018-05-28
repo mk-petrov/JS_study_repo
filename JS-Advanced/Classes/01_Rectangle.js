df4d033c30926c8296922ae4594d181e5b1e9b75
mk-petrov <petko.petrov89@gmail.com>
2018-05-28 14:20:06 +0200

    added new stuff

--- JS-Advanced/Classes/01_Rectangle.js
+++ JS-Advanced/Classes/01_Rectangle.js
@@ -6,6 +6,12 @@ class Rectangle {
         this.height = height;
         this.color = color;
     }
+
+    //no function keyword needed.
+    //Methods perform operations over the class data
+    calcArea(){
+        return this.width * this.height;
+    }
 }
 
 console.log(Rectangle);   //[Function: Rectangle]
@@ -13,3 +19,10 @@ console.log(typeof Rectangle);   // function
 
 let rect = new Rectangle(20, 35, 'green');
 let rect2 = new Rectangle(10, 18);  // color prop is missing by the declaration, but by default is set to white
+
+//in JS the instances of a class can be modify unlike C# or JAVA
+rect.name = 'pesho';
+
+//calcArea will be not displayed because is bind to the prototype, and every instance of that class will use the same function, not a copy of it
+console.log(rect);  //the name property with 'pesho' value will be there
+console.log(rect.calcArea());
