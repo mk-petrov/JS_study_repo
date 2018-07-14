32a11cefc5c61ff8d90454d80cbc7ee91b443135
mk-petrov <petko.petrov89@gmail.com>
2018-07-14 10:26:18 +0200

    SPA no framework used

--- JS-Advanced/Simple SPAs/booklibrary/app.js
+++ JS-Advanced/Simple SPAs/booklibrary/app.js
@@ -13,6 +13,10 @@ $(() => {
     $('#linkCreate').click(() => showView('create'));
     $('#linkLogout').click(() => showView('logout'));
 
+
+    $('#formLogin').submit((e) => {e.preventDefault(); login()});
+    $('#formRegister').submit(register);
+
     // Hide everything and show only the desired one
     function showView(name) {
         $('section').hide();
@@ -25,5 +29,79 @@ $(() => {
             case 'create': $('#viewCreate').show(); break;
             case 'logout': $('#viewLogout').show(); break;
         }
+    }
+
+    function request(uri, method, data) {
+        let req = {
+            url: baseUrl + uri,
+            method: method,
+            headers: {
+                'Authorization': ''
+            }
+        };
+
+        if (data !== undefined){
+            req.data = JSON.stringify(data);
+        }
+
+        return $.ajax(req);
+    }
+    
+    function setStorage(data) {
+        localStorage.setItem('authtoken', data._kmd.authtoken);
+        localStorage.setItem('username', data.username);
+        $('#loggedInUser').text(`Welcome, ${data.username}!`);
+        showView('books');
+    }
+    
+    function login() {
+        let username = $("[name = 'username']").val();
+        let password = $("[name = 'passwd']").val();
+
+        let req = {
+            url: baseUrl + 'user/' + appKey + '/login',
+            method: 'POST',
+            headers: {
+                'Authorization': 'Basic ' + btoa(appKey + ':' + appSecret),
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify({
+                username: username,
+                password: password
+            }),
+            success: setStorage,
+            error: (reason) => console.log(reason)
+        };
+
+        $.ajax(req);
+    }
+
+    function register(e) {
+        e.preventDefault();
+        let username = $("[name = 'usernameR']").val();
+        let password = $("[name = 'passwdR']").val();
+        let passwordRepeat = $("[name = 'passwdRepeat']").val();
+
+        if(password !== passwordRepeat){
+            alert("Passwords don't match!");
+            return;
+        }
+
+        let req = {
+            url: baseUrl + 'user/' + appKey,
+            method: 'POST',
+            headers: {
+                'Authorization': 'Basic ' + btoa(appKey + ':' + appSecret),
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify({
+                username: username,
+                password: password
+            }),
+            success: setStorage,
+            error: (reason) => console.log(reason)
+        };
+
+        $.ajax(req);
     }
 });
